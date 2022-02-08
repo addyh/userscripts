40ec1c7d7a668dd023e596777a54d8b971abeb56
addyh <13260884+addyh@users.noreply.github.com>
2022-02-08 10:04:23 -0500

    Create remove-yt-ads.user.js

---
+++ remove-yt-ads.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Remove YouTube Ads - Search Page
+// @description  Removes ads on youtube searches
+// @version      1.0.0
+// @run-at       document-end
+// @namespace    addyh
+// @copyright    addyh
+// @author       addyh
+// @grant        none
+//
+// @match        *://*.youtube.com/*
+// @match        *://*.yewtu.be/*
+// ==/UserScript==
+
+function loader_loop(){
+
+    // Search Page
+    var ads = [
+        'ytd-promoted-sparkles-web-renderer',
+        'ytd-promoted-sparkles-text-search-renderer'
+    ];
+    for (var ad of ads) {
+        var els = document.querySelectorAll(ad);
+        for (var e of els) {
+            e.style.display = "none";
+            e.remove();
+        }
+    }
+}
+
+let start = window.setInterval(function(){
+    if (document.readyState === "complete") {
+        window.clearInterval(start);
+        loader_loop();
+    }
+}, 10);
+
+let mutationObserver = new MutationObserver(function() {
+    loader_loop();
+});
+
+mutationObserver.observe(document.documentElement, {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+});
