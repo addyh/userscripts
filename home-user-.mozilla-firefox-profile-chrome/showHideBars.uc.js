0268dd4a8f5c0c77e645916b04cef6d51ea29ed9
addyh <13260884+addyh@users.noreply.github.com>
2022-12-15 13:47:05 -0500

    Update showHideBars.uc.js

--- home-user-.mozilla-firefox-profile-chrome/showHideBars.uc.js
+++ home-user-.mozilla-firefox-profile-chrome/showHideBars.uc.js
@@ -18,9 +18,13 @@ UC.showHideBars = {
 
 		document.onkeydown = function(e) {
 
+			// ALT + Z
+			// Show/hide all top bars
 			if (e.key == 'z' && e.altKey) {
 				var t = document.getElementById('titlebar').style.visibility;
-				if ( t == 'collapse' ) {
+				var n = document.getElementById('nav-bar').style.visibility;
+				var b = document.getElementById('PersonalToolbar').style.visibility;
+				if ( t == 'collapse' || n == 'collapse' || b == 'collapse' ) {
 					document.getElementById('titlebar').style.visibility = 'visible';
 					document.getElementById('nav-bar').style.visibility = 'visible';
 					document.getElementById('PersonalToolbar').style.visibility = 'visible';
@@ -31,22 +35,29 @@ UC.showHideBars = {
 				}
 			}
 
-			if (e.key == 'a' && e.altKey) {
+			// ALT + X
+			// Show/hide the title bar (tabs bar)
+			if (e.key == 'x' && e.altKey) {
 				var t = document.getElementById('titlebar').style.visibility;
-				var n = document.getElementById('nav-bar').style.visibility;
-				if ( n == 'collapse' ) {
-
-					if ( t == 'collapse' ) {
-						document.getElementById('titlebar').style.visibility = 'visible';
-					} else {
-						document.getElementById('nav-bar').style.visibility = 'visible';
-						document.getElementById('PersonalToolbar').style.visibility = 'visible';
-					}
+				if ( t == 'collapse' ) {
+					document.getElementById('titlebar').style.visibility = 'visible';
 				} else {
-					document.getElementById('nav-bar').style.visibility = 'collapse';
-					document.getElementById('PersonalToolbar').style.visibility = 'collapse';
+					document.getElementById('titlebar').style.visibility = 'collapse';
 				}
 			}
+
+			// ALT + A
+			// Show/hide the nav bar (address bar)
+			if (e.key == 'a' && e.altKey) {
+				var n = document.getElementById('nav-bar').style.visibility;
+				if ( n == 'collapse' ) {
+					document.getElementById('nav-bar').style.visibility = 'visible';
+				} else {
+					document.getElementById('nav-bar').style.visibility = 'collapse';
+				}
+			}
+
+			// CTRL+SHIFT+B still show/hides the bookmarks bar
 		}
 	}
 }
