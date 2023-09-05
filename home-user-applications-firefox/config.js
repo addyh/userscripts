49354f16b0aa7245212b20434f8d893268f66759
addyh <13260884+addyh@users.noreply.github.com>
2023-09-05 12:24:00 -0400

    Update config.js

--- home-user-applications-firefox/config.js
+++ home-user-applications-firefox/config.js
@@ -1,17 +1,57 @@
 // skip 1st line
-// https://github.com/xiaoxiaoflood/firefox-scripts
-lockPref('xpinstall.signatures.required', false);
-lockPref('extensions.install_origins.enabled', false);
+
+// https://github.com/alice0775/userChrome.js/blob/master/117/install_folder/config.js
+
+/*
+ 2023/07/11 Removed Services.jsm, per Bug 1780695
+ 2022/06/07 remove osfile.jsm
+ 2021/08/05 fix for 92+ port Bug 1723723 - Switch JS consumers from getURLSpecFromFile to either getURLSpecFromActualFile or getURLSpecFromDir
+ 2019/12/11 01:30 fix 72 use "load" in config.js, working with Sub-Script/Overlay Loader v3.0.60mod
+ 2019-10-22 23:00
+*/
 
 try {
-  let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
-  cmanifest.append('utils');
-  cmanifest.append('chrome.manifest');
-  Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
 
-  Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
-} catch (ex) {};
+  let {
+    classes: Cc,
+    interfaces: Ci,
+    utils: Cu
+  } = Components;
+
+  //let { Services } = Cu.import('resource://gre/modules/Services.jsm');
+  //Cu.import('resource://gre/modules/osfile.jsm');
+
+  function UserChrome_js() {
+    Services.obs.addObserver(this, 'domwindowopened', false);
+  };
+
+  UserChrome_js.prototype = {
+    observe: function (aSubject, aTopic, aData) {
+        aSubject.addEventListener('load', this, true);
+    },
+
+    handleEvent: function (aEvent) {
+      let document = aEvent.originalTarget;
+      if (document.location && document.location.protocol == 'chrome:') {
+      const ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+      const fph = ios.getProtocolHandler("file").QueryInterface(Ci.nsIFileProtocolHandler);
+      const ds = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties);
+
+        let file = ds.get("UChrm", Ci.nsIFile);
+        file.append('userChrome.js');
+        let fileURL = fph
+                      .getURLSpecFromActualFile(file) + "?" + file.lastModifiedTime;
+        Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Ci.mozIJSSubScriptLoader)
+                     .loadSubScript(fileURL, document.defaultView, 'UTF-8');
+      }
+    },
+  };
+
+  if (!Cc['@mozilla.org/xre/app-info;1'].getService(Ci.nsIXULRuntime).inSafeMode)
+    new UserChrome_js();
+
+} catch(ex) {};
 
 try {
-  Cu.import('chrome://userchromejs/content/userChrome.jsm');
-} catch (ex) {};
+  pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+} catch(e) {}
