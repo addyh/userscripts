f148dfa43db4fd6b231cae718e91eb2771b62b00
addyh <13260884+addyh@users.noreply.github.com>
2024-01-23 13:30:15 -0500

    update nextcloud-tasks.user.js

--- nextcloud-tasks.user.js
+++ nextcloud-tasks.user.js
@@ -14,17 +14,57 @@
 (function() {
 
     // Close side panel after selecting a Task List on mobile
+    function closeNavSidePanel() {
+        document.querySelector( '.app-navigation-toggle' ).click();
+    }
     window.addEventListener( 'load', function() {
         setTimeout( function() {
             if ( window.innerWidth <= 1024 ) {
-                var elements = document.querySelectorAll( '.app-navigation-entry-link' );
-                for ( var i = 0; i < elements.length; i++ ) {
-                    elements[i].addEventListener( 'click', function() {
-                        document.querySelector( '.app-navigation-toggle' ).click();
-                    } );
+                // Apply to all Task lists
+                var task_lists = document.querySelectorAll( 'li[calendar-id] .app-navigation-entry-link' );
+                for ( var i = 0; i < task_lists.length; i++ ) {
+                    task_lists[i].addEventListener( 'click', closeNavSidePanel);
+                }
+                // Apply to all Collections of lists
+                var collections = document.querySelectorAll( 'li[collection-id] .app-navigation-entry-link' );
+                for ( var i = 0; i < collections.length; i++ ) {
+                    collections[i].addEventListener( 'click', closeNavSidePanel);
                 }
             }
         }, 1000 );
     } );
+
+    // Remove "Delete all completed tasks" button
+    function mutation_loop() {
+        var e = document.querySelector('div.loadmore.reactive > button > span.button-vue__wrapper > span.button-vue__icon > span.material-design-icon.delete-icon');
+        if (
+            e &&
+            e.parentElement &&
+            e.parentElement.parentElement &&
+            e.parentElement.parentElement.parentElement &&
+            e.parentElement.parentElement.parentElement.parentElement
+        ) {
+            e.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+        }
+    }
+    window.addEventListener( 'load', function() {
+        let mutationObserver = new MutationObserver(function() {
+            mutation_loop();
+        });
+        mutationObserver.observe( document.documentElement, {
+            attributes: true,
+            characterData: true,
+            childList: true,
+            subtree: true,
+            attributeOldValue: true,
+            characterDataOldValue: true
+        } );
+    } );
+
+    // @TODO Hide/Show all completed tasks button
+    // Completed tasks:
+    //   .task-item--closed
+    // Completed tasks header:
+    //   heading.heading--hiddentasks.reactive
 
 })();
