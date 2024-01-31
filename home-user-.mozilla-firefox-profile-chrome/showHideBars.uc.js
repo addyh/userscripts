dd19a24b053ee229c52f73e959c4c957e26ed8be
addyh <13260884+addyh@users.noreply.github.com>
2024-01-31 12:53:07 -0500

    Update showHideBars.uc.js - use event listener for keyboard events

--- home-user-.mozilla-firefox-profile-chrome/showHideBars.uc.js
+++ home-user-.mozilla-firefox-profile-chrome/showHideBars.uc.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name            showHideBars
 // @author          addyh
+// @description     Add ALT+A/X/Z shortcuts to show/hide firefox toolbars. (A=address bar, X=title bar, Z=all toolbars)
 // @include         main
 // ==/UserScript==
 
@@ -12,55 +13,47 @@
 	// #navigator-toolbox - Nav Bar, Title Bar, and Bookmarks Bar
 	// CTRL+SHIFT+B still show/hides the bookmarks bar
 
-	// document.getElementById('titlebar').style.visibility = 'collapse';
-	// document.getElementById('nav-bar').style.visibility = 'collapse';
-	// document.getElementById('PersonalToolbar').style.visibility = 'collapse';
+	window.addEventListener('keydown', function (e) {
 
-	setTimeout(function() {
-
-		document.onkeydown = function(e) {
-
-			// ALT + Z
-			// Show/hide all top bars
-			if (e.key == 'z' && e.altKey) {
-				var t = document.getElementById('titlebar').style.visibility;
-				var n = document.getElementById('nav-bar').style.visibility;
-				var b = document.getElementById('PersonalToolbar').style.visibility;
-				if ( t == 'collapse' || n == 'collapse' || b == 'collapse' ) {
-					document.getElementById('titlebar').style.visibility = 'visible';
-					document.getElementById('nav-bar').style.visibility = 'visible';
-					document.getElementById('PersonalToolbar').style.visibility = 'visible';
-				} else {
-					document.getElementById('titlebar').style.visibility = 'collapse';
-					document.getElementById('nav-bar').style.visibility = 'collapse';
-					document.getElementById('PersonalToolbar').style.visibility = 'collapse';
-				}
+		// ALT + Z
+		// Show/hide all top bars
+		if (e.key == 'z' && e.altKey) {
+			var t = document.getElementById('titlebar').style.visibility;
+			var n = document.getElementById('nav-bar').style.visibility;
+			var b = document.getElementById('PersonalToolbar').style.visibility;
+			if ( t == 'collapse' || n == 'collapse' || b == 'collapse' ) {
+				document.getElementById('titlebar').style.visibility = 'visible';
+				document.getElementById('nav-bar').style.visibility = 'visible';
+				document.getElementById('PersonalToolbar').style.visibility = 'visible';
+			} else {
+				document.getElementById('titlebar').style.visibility = 'collapse';
+				document.getElementById('nav-bar').style.visibility = 'collapse';
+				document.getElementById('PersonalToolbar').style.visibility = 'collapse';
 			}
+		}
 
-			// ALT + X
-			// Show/hide the title bar (tabs bar)
-			if (e.key == 'x' && e.altKey) {
-				var t = document.getElementById('titlebar').style.visibility;
-				if ( t == 'collapse' ) {
-					document.getElementById('titlebar').style.visibility = 'visible';
-				} else {
-					document.getElementById('titlebar').style.visibility = 'collapse';
-				}
+		// ALT + X
+		// Show/hide the title bar (tabs bar)
+		if (e.key == 'x' && e.altKey) {
+			var t = document.getElementById('titlebar').style.visibility;
+			if ( t == 'collapse' ) {
+				document.getElementById('titlebar').style.visibility = 'visible';
+			} else {
+				document.getElementById('titlebar').style.visibility = 'collapse';
 			}
+		}
 
-			// ALT + A
-			// Show/hide the nav bar (address bar)
-			if (e.key == 'a' && e.altKey) {
-				var n = document.getElementById('nav-bar').style.visibility;
-				if ( n == 'collapse' ) {
-					document.getElementById('nav-bar').style.visibility = 'visible';
-				} else {
-					document.getElementById('nav-bar').style.visibility = 'collapse';
-				}
+		// ALT + A
+		// Show/hide the nav bar (address bar)
+		if (e.key == 'a' && e.altKey) {
+			var n = document.getElementById('nav-bar').style.visibility;
+			if ( n == 'collapse' ) {
+				document.getElementById('nav-bar').style.visibility = 'visible';
+			} else {
+				document.getElementById('nav-bar').style.visibility = 'collapse';
 			}
+		}
 
-		};
-
-	}, 1000);
+	});
 
 })();
