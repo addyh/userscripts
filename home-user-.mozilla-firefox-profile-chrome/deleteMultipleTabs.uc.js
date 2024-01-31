6c949f6f768632efc0bb95e53fdcfe62a982b023
addyh <13260884+addyh@users.noreply.github.com>
2024-01-31 12:53:42 -0500

    Update deleteMultipleTabs.uc.js - Update deleteMultipleTabs.uc.js - use event listener for keyboard events

--- home-user-.mozilla-firefox-profile-chrome/deleteMultipleTabs.uc.js
+++ home-user-.mozilla-firefox-profile-chrome/deleteMultipleTabs.uc.js
@@ -1,22 +1,19 @@
 // ==UserScript==
 // @name            deleteMultipleTabs
 // @author          addyh
+// @description     After ctrl+ or shift+ clicking multiple tabs, allow pressing the Delete key to close those tabs.
 // @include         main
 // ==/UserScript==
 
 (function() {
 
-	setTimeout(function() {
+	window.addEventListener('keydown', function (e) {
 
-		document.onkeydown = function(e) {
+		if (e.key == 'Delete' && gBrowser.multiSelectedTabsCount) {
+			gBrowser.removeMultiSelectedTabs();
+			return false;
+		}
 
-			if (e.key == 'Delete' && gBrowser.multiSelectedTabsCount) {
-				gBrowser.removeMultiSelectedTabs();
-				return false;
-			}
-
-		};
-
-	}, 1000);
+	});
 
 })();
