08edbf600c99f5474977477d3d8e1f43948602d3
addyh <13260884+addyh@users.noreply.github.com>
2024-04-18 16:20:12 -0400

    update kinsta.user.js - v1.0.2

--- kinsta.user.js
+++ kinsta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kinsta
 // @description  MyKinsta Fixes
-// @version      1.0.1
+// @version      1.0.2
 // @author       addyh
 // @copyright    GPLv3
 // @updateURL    https://github.com/addyh/userscripts/raw/master/kinsta.user.js
@@ -18,35 +18,47 @@
     window.addEventListener( 'load', function() {
 
         function mutation_loop() {
-            var str = document.title.substring( 0, document.title.length - 11 ).replace( /\./g, '' );
-            var style = document.getElementById('userscript-kinsta-fix-dropdown-background');
-            if ( !style ) {
-                style = document.createElement('style');
-                style.id = "userscript-kinsta-fix-dropdown-background";
-                document.head.append(style);
+            // Change Current Site Dropdown Element Background Color to Tell it Apart
+            var current_site_id;
+            var dropdown_elements = document.querySelectorAll("#\\:r2\\: div[name]");
+            if ( dropdown_elements ) {
+                for ( var current_site_dropdown_element of dropdown_elements ) {
+                    if ( window.location.pathname.includes( current_site_dropdown_element.id ) ) {
+                        current_site_id = current_site_dropdown_element.id;
+                    }
+                }
+                var style = document.getElementById('userscript-kinsta-fix-dropdown-background');
+                if ( !style ) {
+                    style = document.createElement('style');
+                    style.id = "userscript-kinsta-fix-dropdown-background";
+                    document.head.append(style);
+                }
+                if ( current_site_id ) {
+                    style.innerHTML = `
+                        div[id="`+current_site_id+`"] {
+                            background-color: #333;
+                            border: 3px solid #333;
+                        }
+                        div[id="`+current_site_id+`"]:hover {
+                            background-color: #000;
+                        }
+                    `;
+                }
             }
-            style.innerHTML = `
-                div[name="`+str+`"] {
-                  background-color: #333;
-                  border: 3px solid #333;
-                }
-                div[name="`+str+`"]:hover {
-                  background-color: #000;
-                }
-            `;
-            document.head.append(style);
         }
-        let mutationObserver = new MutationObserver( function() {
-            mutation_loop();
-        } );
-        mutationObserver.observe( document.getElementById(':r2:'), {
-            attributes: true,
-            characterData: true,
-            childList: true,
-            subtree: true,
-            attributeOldValue: true,
-            characterDataOldValue: true
-        } );
+        if ( document.getElementById(':r2:') ) {
+            let mutationObserver = new MutationObserver( function() {
+                mutation_loop();
+            } );
+            mutationObserver.observe( document.getElementById(':r2:'), {
+                attributes: true,
+                characterData: true,
+                childList: true,
+                subtree: true,
+                attributeOldValue: true,
+                characterDataOldValue: true
+            } );
+        }
 
     } );
 })();
