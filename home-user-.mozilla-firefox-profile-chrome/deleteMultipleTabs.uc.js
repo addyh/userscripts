aa2ff057890e4b481b516b75a10c9164be7861d2
addyh <13260884+addyh@users.noreply.github.com>
2024-12-01 11:44:34 -0500

    update userChromes

--- home-user-.mozilla-firefox-profile-chrome/deleteMultipleTabs.uc.js
+++ home-user-.mozilla-firefox-profile-chrome/deleteMultipleTabs.uc.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name            deleteMultipleTabs
+// @version         2024.12.1.111204
 // @author          addyh
 // @description     After ctrl+ or shift+ clicking multiple tabs, allow pressing the Delete key to close those tabs.
 // @include         main
--- home-user-.mozilla-firefox-profile-chrome/showHideBars.uc.js
+++ home-user-.mozilla-firefox-profile-chrome/showHideBars.uc.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name            showHideBars
+// @version         2024.12.1.111526
 // @author          addyh
 // @description     Add ALT+A/X/Z shortcuts to show/hide firefox toolbars. (A=address bar, X=title bar, Z=all toolbars)
 // @include         main
@@ -7,7 +8,7 @@
 
 (function() {
 
-	// #titlebar - Title/Tabs Bar
+	// #TabsToolbar - Tabs Title Bar
 	// #nav-bar - Nav/Address Bar
 	// #PersonalToolbar - Bookmarks Bar
 	// #navigator-toolbox - Nav Bar, Title Bar, and Bookmarks Bar
@@ -15,7 +16,7 @@
 
 	window.addEventListener('keydown', function (e) {
 
-		var titlebar = document.getElementById('titlebar').style.visibility;
+		var titlebar = document.getElementById('TabsToolbar').style.visibility;
 		var navbar = document.getElementById('nav-bar').style.visibility;
 		var bookmarksbar = document.getElementById('PersonalToolbar').style.visibility;
 
@@ -53,7 +54,7 @@
 			}
 		}
 
-		document.getElementById('titlebar').style.visibility = titlebar;
+		document.getElementById('TabsToolbar').style.visibility = titlebar;
 		document.getElementById('nav-bar').style.visibility = navbar;
 		document.getElementById('PersonalToolbar').style.visibility = bookmarksbar;
 
