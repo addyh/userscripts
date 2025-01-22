3d77d60824915a5bba2cae2fee563bfe0d4cb356
addyh <13260884+addyh@users.noreply.github.com>
2025-01-22 11:55:43 -0500

    update userscripts

--- chatgpt.user.js
+++ chatgpt.user.js
@@ -1,29 +1,29 @@
 // ==UserScript==
-// @name         ChatGPT - Project Pages
-// @description  ChatGPT - Project Page Fixes
-// @version      2025.1.18.134241
+// @name         ChatGPT
+// @description  ChatGPT Fixes
+// @version      2025.1.22.115318
 // @author       addyh
 // @copyright    GPLv3
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
-// @updateURL    https://github.com/addyh/userscripts/raw/master/chatgpt-project-pages.user.js
-// @downloadURL  https://github.com/addyh/userscripts/raw/master/chatgpt-project-pages.user.js
+// @updateURL    https://github.com/addyh/userscripts/raw/master/chatgpt.user.js
+// @downloadURL  https://github.com/addyh/userscripts/raw/master/chatgpt.user.js
 // @supportURL   https://github.com/addyh/userscripts/issues
 // @homepage     https://github.com/addyh/userscripts
 // @run-at       document-start
 // @grant        none
 //
-// @match        https://*.chatgpt.com/*/project
+// @match        https://*.chatgpt.com/*
 // ==/UserScript==
 
 (function() {
 
-	let style_id = 'userscript-chatgpt-project-pages-custom-css';
+	let style_id = 'userscript-chatgpt-custom-css';
 	let style = document.getElementById( style_id );
 	if ( !style ) {
 		style = document.createElement('style');
 		style.id = style_id;
 		style.innerHTML = `
-			/* Remove Last Chat Message Preview on Project Page */
+			/* Remove Last Chat Message Preview on Project Pages */
 			ol > li > a .truncate.text-token-text-secondary {
 				display: none !important;
 			}
