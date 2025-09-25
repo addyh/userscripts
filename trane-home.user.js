b79b2ea4144b548ca8a795a9a952fecf2d8c0e6c
addyh <13260884+addyh@users.noreply.github.com>
2025-09-25 02:49:32 -0400

    fix trane script

--- trane-home.user.js
+++ trane-home.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trane Home
 // @description  Trane Home Login Page - Fix Autofill
-// @version      2025.05.21.171722
+// @version      2025.09.5.024850
 // @author       addyh
 // @copyright    GPLv3
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tranehome.com
@@ -9,7 +9,7 @@
 // @downloadURL  https://github.com/addyh/userscripts/raw/master/trane-home.user.js
 // @supportURL   https://github.com/addyh/userscripts/issues
 // @homepage     https://github.com/addyh/userscripts
-// @run-at       document-start
+// @run-at       document-end
 // @grant        none
 //
 // @match        https://*.tranehome.com/login
