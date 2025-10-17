20d56df9a190985ecc22f138fb54e8ee8df452b5
addyh <13260884+addyh@users.noreply.github.com>
2025-10-17 15:23:33 -0400

    update

--- youtube-ads.user.js
+++ youtube-ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Ads
 // @description  Removes YouTube Ads
-// @version      2025.10.17.151742
+// @version      2025.10.17.152304
 // @author       addyh
 // @copyright    GPLv3
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
@@ -18,7 +18,7 @@
 function loader_loop() {
 
 	// Search Page
-	var ads = [
+	var remove = [
 		'ytd-display-ad-renderer',
 		'ytm-display-ad-renderer',
 		'ytd-promoted-sparkles-web-renderer',
@@ -28,16 +28,24 @@ function loader_loop() {
 		'ytd-clarification-renderer',
 		'ytm-clarification-renderer',
 
-		// Shorts and Posts
-		'ytd-rich-section-renderer',
-		'ytm-rich-section-renderer',
-
 		// These interfere with the edit comment popup
 		// 'ytd-popup-container',
 		// 'ytm-popup-container',
 	];
-	for ( var ad of ads ) {
-		var els = document.querySelectorAll( ad );
+	var hide = [
+		// Shorts and Community Posts
+		'ytd-rich-section-renderer',
+		'ytm-rich-section-renderer',
+	];
+	for ( var el of remove ) {
+		var els = document.querySelectorAll( el );
+		for ( var e of els ) {
+			e.style.display = "none";
+			e.remove();
+		}
+	}
+	for ( var el of hide ) {
+		var els = document.querySelectorAll( el );
 		for ( var e of els ) {
 			e.style.display = "none";
 			// e.remove();
